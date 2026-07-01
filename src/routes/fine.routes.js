dae29c4df2e519585122ca30ca73d1800c6c21b3
Rishav kumar <rishavkumar@Rishavs-MacBook-Air-2.local>
2026-07-01 10:08:14 +0530

    feat: v1.2 fine management - calculate and track late return fines

---
+++ src/routes/fine.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const {getMyFines, payFine, getAllFines} = require("../controllers/fine.controller");
+const {protect} = require("../middlewares/auth.middleware");
+
+const router = express.Router();
+
+router.get("/my",protect, getMyFines);
+router.get("/:id/pay", protect,payFine );
+router.get("/",protect,getAllFines);
+
+module.exports = router;
